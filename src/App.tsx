2b9db95c74c09cf4d1274a30049d68a75eeb0820
drogon98 <h21948298@gmail.com>
2022-07-22 05:50:56 +0300

    added dates state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import "./index.css";
 import Calendar from "./components/Calendar";
+import { useState } from "react";
 
 function App() {
-  return <Calendar startDate={new Date().getTime()} />;
+  const [dates, setDates] = useState<{
+    startDate: number | undefined;
+    endDate: number | undefined;
+  }>();
+  // console.log("dates :>> ", dates);
+  return (
+    <Calendar
+      // startDate={new Date().getTime()}
+      startDate={1582934400000}
+      setDates={setDates}
+    />
+  );
 }
 
 export default App;
